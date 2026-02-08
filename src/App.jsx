fd0991f96b4ec8dec1051c99718619576a3d89c9
N Elie <ntezielie@gmail.com>
2026-02-08 10:20:33 +0000

    add names and social media

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Elie Nteziryayo",
+  title: "Software Engineer",
+  email: "ntezielie@gmail.com",
+  gitHub: "https://github.com/nelie250",
+  instagram: "https://www.instagram.com/elie_n._/",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://x.com/elie_n250",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
